d7ecb48bf3eb3d38047e38f3bf1d42b08792646e
M. Doni Saputra <doni.alfatih@gmail.com>
2022-02-04 16:08:50 +0700

    Pertemuan-2

---
+++ pertemuan2/index.php
@@ -0,0 +1,21 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+echo "M. Doni Saputra";
+echo "<br>";
+print "M. Doni Saputra";
+echo "<br>";
+print_r ("M. Doni Saputra");
+echo "<br>";
+var_dump ("M.Doni Saputra");
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+
+?>
