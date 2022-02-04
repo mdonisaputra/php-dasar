0c2ddb44e7fe287535a28d2ccbed7c587e63d059
M. Doni Saputra <doni.alfatih@gmail.com>
2022-02-04 16:30:53 +0700

    Edit Pertemuan-2

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -7,6 +7,7 @@
 // print_r
 // var_dump
 
+/*
 echo "M. Doni Saputra";
 echo "<br>";
 print "M. Doni Saputra";
@@ -14,8 +15,41 @@ echo "<br>";
 print_r ("M. Doni Saputra");
 echo "<br>";
 var_dump ("M.Doni Saputra");
+*/
 
 // Penulisan sintaks PHP
 // 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+
+// $nama = "Doni";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+ $x = 10;
+ $y = 20;
+ echo $x * $y;
+
+// penggabung string 
 
 ?>
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Belajar PHP</title>
+</head>
+<body>
+	<h1>Halo, Selamat Datang <?php echo $nama; ?></h1>
+	<p><?php echo "Ini adalah paragraf"; ?></p>
+</body>
+</html>
+
+-->
