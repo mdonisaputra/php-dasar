4fb56e34e681746f095fac929c9038ba44ac36d8
M. Doni Saputra <doni.alfatih@gmail.com>
2022-02-05 21:10:06 +0700

    Pertemuan-5 Selesai

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -1,5 +1,32 @@
 <?php
 // array
 // variabel yang dapat memiliki banyak nilai
+// elemen pada array boleh memiliki tipe data yang berbeda
+// pasangan antara key dan value
+// key-nya adalah index, yang dimulai dari 0
+
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu", "Kamis");
+// cara baru
+$bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni"]; 
+$arr1 = [123, "tulisan", false];
+
+// Cara Menampilkan Array
+// var_dump() / print_r()
+var_dump($hari);
+echo "<br>";
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+
+// Menampilkan 1 Elemen pada Array
+echo $arr1[0];
+echo "<br>";
+echo $bulan[1];
+echo "<br>";
+// Menambahkan elemen baru pada array
+$hari[] = "Jum'at";
+var_dump($hari);
 
 ?>
---
+++ pertemuan5/latihan3.php
@@ -0,0 +1,28 @@
+<?php
+$mahasiswa = [
+    ["M. Doni Saputra", 17100051, "Teknik Informatika", "doni.alfatih@gmail.com"], ["Siti Fatimah", 16100075, "Akuntansi", "sitifatimah@gmail.com"]
+]; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    
+<!--Menampilkan Array Mahasiswa Menggunakan Looping -->
+<?php foreach ($mahasiswa as $mhs) : ?>
+    <ul>
+        <li>Nama : <?= $mhs[0]; ?></li>
+        <li>NRP : <?= $mhs[1]; ?></li>
+        <li>Jurusan : <?= $mhs[2]; ?></li>
+        <li>Email : <?= $mhs[3]; ?></li> 
+    </ul>
+<?php endforeach; ?>   
+
+</body>
+</html>
