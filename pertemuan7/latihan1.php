dbe2cbbfe7fdfc48cde49ea8d159120985a78e08
M. Doni Saputra <doni.alfatih@gmail.com>
2022-02-05 22:55:23 +0700

    Pertemuan-7

---
+++ pertemuan7/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+/* Variable Scope / lingkup variabel
+$x = 10;
+function tampilx() {
+    global $x;
+    echo $x;
+}
+
+tampilx();
+*/
+
+// SUPERGLOBALS
+// variable global milik PHP merupakan Array Associative
+// var_dump($_SERVER);
+
+18.23
+?>
